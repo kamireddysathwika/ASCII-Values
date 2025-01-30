069cfe55e85657a2324c865bf3cc4b04be6d36d8
kamireddysathwika <kamireddysathwika0908@gmail.com>
2025-01-30 14:23:27 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,15 @@
+public class ASCIIValue {
+    public static void main(String[] args) {
+        // Predefined character
+        char character = 'A';
+        
+        // Get the ASCII value of the character
+        int asciiValue = (int) character;
+        
+        // Display the character and its ASCII value
+        System.out.printf("Character: " +character);
+        System.out.println("ASCII value: "  +asciiValue);
+    }
+}
+
+
